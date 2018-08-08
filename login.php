baea67c1304a8e72b72c30eb59fff08f1471844c
EllyDeKlein <39516243+EllyDeKlein@users.noreply.github.com>
2018-08-08 14:30:13 +0200

    Login werkt

--- login.php
+++ login.php
@@ -1,74 +1,32 @@
 <?php
-
- require_once('verbinding.php');
-  ?>
+require_once('verbinding.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <style>
     .row div{padding: 20px 10px;border: 1px solid;}
     </style>
   </head>
   <body>
-
-
-
       <div class='jumbotron jumbotron-fluid bg-success text-white text-center' style="background: #aaa;">
-
         <div class="container">
         <h1> Register for Kameleon</h1>
         <p>Free colors of pay</p>
       </div>
 </div>
-
-<!--- cards --->
-
-<div class="container">
-
-<!--- cards --->
-<div class="card-deck">
-  <div class="card">
-    <img class="card-img-top img-fluid" src="img/Logo.svg" alt="Card image cap">
-    <div class="card-block">
-      <h4 class="card-title">Login!</h4>
-      <p class="card-text"> Or register for free of 1 euro per minuut!</p>
-    </div>
-
-  </div>
-
-
-    <form action='sqlregister.php' method='POST' enctype="multipart/form-data">
-          <div class="test">
-            <label>Username</label> <input type='text' name='rusername' value=''><br>
-          </div>
-          <div class="test">
-            <label>Email</label> <input type='text' name='remail' value=''><br>
-          </div>
-          <div class="test">
-            <label>Password</label> <input type='password' name='rpassword' value=''><br>
-          </div>
-          <div class="test">
-            <label>Confirm password</label>
-            <input type="password" name='rpassword_2'>
-          </div>
-          <div class="test">
-            <input type='submit' name='submit' class='knop'><br>
-          </div>
-    </form>
-
-
+  <form action="loginscript.php" method="post" > <!--- original is test.php --->
+    <h2>Login</h2>
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password"  placeholder="Password">
+    <button class="btn" type="submit" name="login_btn">Sign in</button>
+  </form>
 <?php
-
 var_dump($_POST);
-
  ?>
-
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
--- sqlregister.php
+++ sqlregister.php
@@ -5,11 +5,8 @@
   echo "connected to database";
   if(isset($_POST) & !empty($_POST))
   {
-
+  $username			     = mysqli_real_escape_string($conn, $_POST['rusername']);
   $email			       = mysqli_real_escape_string($conn, $_POST['remail']);
-  $adres			       = mysqli_real_escape_string($conn, $_POST['radres']);
-  $woonplaats        = mysqli_real_escape_string($conn, $_POST['rwoonplaats']);
-  $iban              = mysqli_real_escape_string($conn, $_POST['riban']);
   $password	         = ($_POST['rpassword']);
   $passwordagain	   = ($_POST['rpassword_2']);
 
@@ -17,9 +14,9 @@
 
 
   { //check double entries voor email
-  $emailsql = "SELECT * FROM `users` WHERE email='$email'";
-  $emailres = mysqli_query($conn, $emailsql);
-  $count = mysqli_num_rows($emailres); //if count equals to 1 then we are not oging to insert this data  mysqlinumrows expects 1 argument
+  $usernamesql = "SELECT * FROM `users` WHERE username='$username'";
+  $usernameres = mysqli_query($conn, $usernamesql);
+  $count = mysqli_num_rows($usernameres); //if count equals to 1 then we are not oging to insert this data  mysqlinumrows expects 1 argument
   if($count == 1){
   echo $fmsg = "Email exists in onze database";
   }
@@ -29,7 +26,7 @@
   if($emailcount == 1){
   echo $fmsg = "Email exists in onze database, please reset je wachtwoord";
   }
-  $sqltest 	= "INSERT INTO `users` (email, password, adres, woonplaats, iban) VALUES ('$email', '$password', '$adres', '$woonplaats', '$iban')";
+  $sqltest 	= "INSERT INTO `users` (username, email, password) VALUES ('$username', '$email', '$password')";
   $result 		= mysqli_query($conn, $sqltest) or die(mysqli_error($conn)); //is used to put it in to database table ? die ik nu in een result $variabel stop
   if($result)
   {
